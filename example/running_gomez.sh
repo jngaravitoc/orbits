f0d84d33764b905aec60c1df69bf6968ba893586
jngaravitoc <nico.garavito@gmail.com>
2015-12-01 12:10:26 -0700

    update

--- example/running_gomez.sh
+++ example/running_gomez.sh
@@ -1,2 +1,7 @@
 #this example reproduce Fig 1 of gomez et al 2015.
-python ../src/main.py gomez15fig1.param
+python ../src/main.py lmc_3E10_mw1E12.param
+python ../src/main.py lmc_5E10_mw1E12.param
+python ../src/main.py lmc_8E10_mw1E12.param
+python ../src/main.py lmc_1E11_mw1E12.param
+python ../src/main.py lmc_1.8E11_mw1E12.param
+python ../src/main.py lmc_2.5E11_mw1E12.param
