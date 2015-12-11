16e374cc19c791829f3b6d828740b622155ef37c
jngaravitoc <jngaravitoc@email.arizona.edu>
2015-12-11 12:11:53 -0700

    update

--- example/sag.sh
+++ example/sag.sh
@@ -1,4 +1,5 @@
-python ../src/main.py sag_1.param
-python ../src/main.py sag_2.param
-python ../src/main.py sag_3.param
-python ../src/main.py sag_4.param
+#python ../src/main.py sag_1.param
+#python ../src/main.py sag_2.param
+#python ../src/main.py sag_3.param
+#python ../src/main.py sag_4.param
+python ../src/main.py sag_heavy.param
