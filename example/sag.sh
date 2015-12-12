686488d54055217fc90d4c7c188dc4c9419931b2
jngaravitoc <nico.garavito@gmail.com>
2015-12-11 17:09:35 -0700

    params update for sag

--- example/sag.sh
+++ example/sag.sh
@@ -1,5 +1,5 @@
 #python ../src/main.py sag_1.param
 #python ../src/main.py sag_2.param
 #python ../src/main.py sag_3.param
-#python ../src/main.py sag_4.param
-python ../src/main.py sag_heavy.param
+python ../src/main.py sag_4.param
+#python ../src/main.py sag_heavy.param
