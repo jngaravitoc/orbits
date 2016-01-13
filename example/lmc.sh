1b147cd4a54248de2037c1869e66309d5ce14034
jngaravitoc <nico.garavito@gmail.com>
2016-01-12 22:22:55 -0700

    lmc script

---
+++ example/lmc.sh
@@ -0,0 +1,5 @@
+python ../src/main.py lmc_1E11_mw1E12_a05.param
+python ../src/main.py lmc_1E11_mw1E12_a1.param
+#python ../src/main.py sag_3.param
+#python ../src/main.py sag_4.param
+#python ../src/main.py sag_heavy.param
