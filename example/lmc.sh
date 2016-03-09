a97d8e280b480c8970fd5c053ac077eaae49f97b
jngaravitoc <jngaravitoc@email.arizona.edu>
2016-03-08 23:14:59 -0700

    script

--- example/lmc.sh
+++ example/lmc.sh
@@ -1,5 +1,41 @@
-python ../src/main.py lmc_1E11_mw1E12_a05.param
-python ../src/main.py lmc_1E11_mw1E12_a1.param
+#python ../src/main.py lmc_1E11_mw1E12_a06.param
+#python ../src/main.py lmc_1E11_mw1E12_a07.param
+#python ../src/main.py lmc_1E11_mw1E12_a15.param
+#python ../src/main.py lmc_1E11_mw1E12_a16.param
+#python ../src/main.py lmc_1E11_mw1E12_a17.param
+
+
 #python ../src/main.py sag_3.param
 #python ../src/main.py sag_4.param
 #python ../src/main.py sag_heavy.param
+
+#python ../src/main.py lmc_1E11_mw1E12-fit-a1.param
+#python ../src/main.py lmc_1E11_mw1E12-fit-a0707.param
+#python ../src/main.py lmc_1E11_mw1E12-fit-a12.param
+#python ../src/main.py lmc_1E11_mw1E12-fit-a15.param
+#python ../src/main.py lmc_1E11_mw1E12-fit-a80.param
+
+#python ../src/main.py lmcwperia13.param
+#python ../src/main.py lmcwperia14.param
+#python ../src/main.py lmcwperia15.param
+
+
+#python ../src/main.py lmcmw_12H6.param
+#python ../src/main.py lmcmw_13H6.param
+#python ../src/main.py lmcmw_16H6.param
+#python ../src/main.py lmcmw_17H6.param
+
+#python ../src/main.py mw1lmc4-07.param
+#python ../src/main.py mw1lmc4-1.0.param
+#python ../src/main.py mw1lmc4-1.2.param
+#python ../src/main.py mw1lmc4-1.4.param
+#python ../src/main.py mw1lmc4-1.6.param
+
+#python ../src/main.py LMC1_MW1df85.param
+#python ../src/main.py LMC1_MW1df8.param
+#python ../src/main.py LMC1_MW1df9.param
+#python ../src/main.py LMC1_MW1df95.param
+#python ../src/main.py LMC1_MW1NFW8.param
+#python ../src/main.py LMC1_MW1NFW85.param
+#python ../src/main.py LMC1_MW1NFW9.param
+python ../src/main.py LMC1_MW1NFW95.param
