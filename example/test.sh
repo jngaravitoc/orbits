0cd8d3fb8c6779adacc63232acb91aac57f6749d
jngaravitoc <nico.garavito@gmail.com>
2017-01-15 14:31:53 -0700

    test.sh

---
+++ example/test.sh
@@ -0,0 +1,7 @@
+#this example reproduce Fig 1 of gomez et al 2015.
+python ../src/main.py gomez14_test/lmc_3E10_mw1E12.param
+python ../src/main.py gomez14_test/lmc_5E10_mw1E12.param
+python ../src/main.py gomez14_test/lmc_8E10_mw1E12.param
+python ../src/main.py gomez14_test/lmc_1E11_mw1E12.param
+python ../src/main.py gomez14_test/lmc_1.8E11_mw1E12.param
+python ../src/main.py gomez14_test/lmc_2.5E11_mw1E12.param
